b498ab8505a6a3c5937327d13458ef2eb5b5d4ac
ViktoriyaPavlova <ophtalma@yandex.ru>
2024-04-18 16:45:02 +0300

    Добавила комментарии к отрисовке таймера

--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
 window.addEventListener("DOMContentLoaded", function () {
   "use strict"; // событие срабатывает тогда, когда полностью подгузилось ДОМ-дерево
-  let tab = document.querySelectorAll(".info-header-tab"),// доступ к заголовкам табов
-    info = document.querySelector(".info-header"),//доступ к родителю табов
+  let tab = document.querySelectorAll(".info-header-tab"), // доступ к заголовкам табов
+    info = document.querySelector(".info-header"), //доступ к родителю табов
     tabContent = document.querySelectorAll(".info-tabcontent"); //доступ ко всем отдельным табам
 
-
-// функция скрывания табов
-  function hideTabContent(a) { //один технический аргумент а
+  // функция скрывания табов
+  function hideTabContent(a) {
+    //один технический аргумент а
     for (let i = a; i < tabContent.length; i++) {
       tabContent[i].classList.remove("show"); //удаляем класс show
-      tabContent[i].classList.add("hide");//добавляем класс hide
+      tabContent[i].classList.add("hide"); //добавляем класс hide
     }
   }
 
   hideTabContent(1); // вызываем функцию, оставляем 1 таб видимым
 
   // функция показывания табов
-  function showTabContent(b) { //один технический аргумент в
-    if (tabContent[b].classList.contains("hide")) { //если таб содержит класс hide
+  function showTabContent(b) {
+    //один технический аргумент в
+    if (tabContent[b].classList.contains("hide")) {
+      //если таб содержит класс hide
       tabContent[b].classList.remove("hide"); // удаляем класс hide
       tabContent[b].classList.add("show"); //добавляем класс show
     }
@@ -26,9 +28,11 @@ window.addEventListener("DOMContentLoaded", function () {
   //вешаем обработчик события на родителя
   info.addEventListener("click", function (event) {
     let target = event.target; //текущий элемент
-    if (target && target.classList.contains("info-header-tab")) { //проверяем, что кликнули на нужный элемент
+    if (target && target.classList.contains("info-header-tab")) {
+      //проверяем, что кликнули на нужный элемент
       for (let i = 0; i < tab.length; i++) {
-        if (target == tab[i]) { //если текущий элемент совпадает с табом
+        if (target == tab[i]) {
+          //если текущий элемент совпадает с табом
           hideTabContent(0); // прячем все табы
           showTabContent(i); // показываем совпадающий таб
           break; //останавливаем цикл
@@ -39,14 +43,17 @@ window.addEventListener("DOMContentLoaded", function () {
 
   // Timer
 
-  let deadline = "2024-06-21";
+  let deadline = "2024-06-21"; //время, до которого отсчитывает таймер, любое нужное
 
+  // функция для подсчета промежутка времени между текущей датой и deadLine
   function getTimeRemaining(endtime) {
-    let t = Date.parse(endtime) - Date.parse(new Date()),
-      seconds = Math.floor((t / 1000) % 60),
-      minutes = Math.floor((t / 1000 / 60) % 60),
-      hours = Math.floor(t / (1000 * 60 * 60));
+    let t = Date.parse(endtime) - Date.parse(new Date()), //переменная для подсчета разницы между deadline и текущей датой в милисекундах
+      seconds = Math.floor((t / 1000) % 60), //количество секунд из милисекунд, округленное до целого числа; далее получаем остаток от целой минуты
+      minutes = Math.floor((t / 1000 / 60) % 60), // количество минут
+      hours = Math.floor(t / (1000 * 60 * 60)); // количество часов
+    // days = Math.floor(t / (1000 * 60 * 60 * 24)) //количество дней
 
+    // возвращаем объект
     return {
       total: t,
       hours: hours,
@@ -55,13 +62,16 @@ window.addEventListener("DOMContentLoaded", function () {
     };
   }
 
+  // функция, которая делает статичную верстку динамичной
   function setClock(id, endtime) {
-    let timer = document.getElementById(id),
+    let timer = document.getElementById(id),// получаем элемент таймер
       hours = timer.querySelector(".hours"),
       minutes = timer.querySelector(".minutes"),
       seconds = timer.querySelector(".seconds"),
-      timeInterval = setInterval(updateClock, 1000);
+      timeInterval = setInterval(updateClock, 1000); // добавляем интервал в 1 секунду
 
+
+      // функция, обновляющая таймер каждую секунду
     function updateClock() {
       let t = getTimeRemaining(endtime);
 
@@ -71,11 +81,11 @@ window.addEventListener("DOMContentLoaded", function () {
         } else return num;
       }
 
-      hours.textContent = addZero(t.hours);
-      minutes.textContent = addZero(t.minutes);
-      seconds.textContent = addZero(t.seconds);
+      hours.textContent = addZero(t.hours); //количество оставшихся часов
+      minutes.textContent = addZero(t.minutes); // количество оставшихся минут
+      seconds.textContent = addZero(t.seconds); // количество оставшихся минут
 
-      if (t.total <= 0) {
+      if (t.total <= 0) { //условие, при котором таймер остановится
         clearInterval(timeInterval);
         hours.textContent = "00";
         minutes.textContent = "00";
@@ -84,5 +94,5 @@ window.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  setClock("timer", deadline);
+  setClock("timer", deadline); //вызываем функцию динамической отрисовки таймера
 });
